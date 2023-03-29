f1b26d7758a524f84673a8e0fc448ecd9c2f4cf3
quguiren <quguiren@huawei.com>
2023-03-29 18:53:45 +0800

    Cli util Adaptation Stage module template
    Signed-off-by: quguiren <quguiren@huawei.com>

---
+++ cli/templates/ets_stage/source/entry/src/main/ets/Application/AbilityStage.ts
@@ -0,0 +1,7 @@
+import AbilityStage from '@ohos.app.ability.AbilityStage';
+
+export default class MyAbilityStage extends AbilityStage {
+    onCreate() {
+        console.log("[Demo] MyAbilityStage onCreate")
+    }
+}
