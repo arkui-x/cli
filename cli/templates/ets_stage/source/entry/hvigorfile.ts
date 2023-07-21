d87a5b7a44b0f030dd7c152846902a66c094c120
coldfresh <linzhen11@huawei.com>
2023-07-21 16:09:37 +0800

    change to ide arkui-x project template
    Signed-off-by: coldfresh <linzhen11@huawei.com>

--- cli/templates/ets_stage/source/entry/hvigorfile.ts
+++ cli/templates/ets_stage/source/entry/hvigorfile.ts
@@ -1,2 +1,2 @@
 // Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
-export { hapTasks } from '@ohos/hvigor-ohos-plugin';
+export { HapTasks } from '@ohos/hvigor-ohos-arkui-x-plugin';
