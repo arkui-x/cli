97c2aa439fbdf0e3422ac542dbc3d32328a1c5e4
quguiren <quguiren@huawei.com>
2023-10-13 14:49:39 +0800

    help show sequence adjustment
    Signed-off-by: quguiren <quguiren@huawei.com>

--- ace_tools/lib/src/cli/ace-help/index.js
+++ ace_tools/lib/src/cli/ace-help/index.js
@@ -9,8 +9,21 @@ function help(commandsSort) {
       helpText += `  ${command._name}   \t\t ${command._description}\n`;
     });
   }
-  program.addHelpText("after", `\nRun "ace help <command>" for more information about a command.`);
+  program.addHelpText("after", `\nRun "ace [command] --help" for more information about a command.`);
   return helpText;
 }
 
-module.exports = help;
+function unknownOptions() {
+  const unknownOption = program.args.filter(arg => arg.startsWith('-'));
+  console.log('\x1B[31m%s\x1B[0m', `Could not find an option with short name "${unknownOption}".\n\n`);
+  console.log('\x1B[31m%s\x1B[0m', `Run 'ace [command] --help' for available ace commands and options.`);
+  process.exit(1);
+}
+
+function unknownCommands(unknownCommand) {
+  console.log('\x1B[31m%s\x1B[0m', `Could not find a command named "${unknownCommand}".\n\n`);
+  console.log('\x1B[31m%s\x1B[0m', `Run 'ace [command] --help' for available ace commands and options.`);
+  process.exit(1);
+}
+
+module.exports = { help, unknownOptions, unknownCommands }
