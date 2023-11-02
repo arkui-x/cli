b4cb44d638bd7b7d6ca5ffca3fa7350847985246
quguiren <quguiren@huawei.com>
2023-11-02 13:21:54 +0800

    modify harmonyos hdc path
    Signed-off-by: quguiren <quguiren@huawei.com>

--- ace_tools/lib/src/cli/ace-build/ace-packager/index.js
+++ ace_tools/lib/src/cli/ace-build/ace-packager/index.js
@@ -382,7 +382,8 @@ function buildiOS(cmd) {
   cmds.forEach(cmd => {
     try {
       exec(cmd, {
-        encoding: 'utf-8'
+        encoding: 'utf-8',
+        stdio: 'pipe'
       });
     } catch (error) {
       if (error.stdout !== null) {
