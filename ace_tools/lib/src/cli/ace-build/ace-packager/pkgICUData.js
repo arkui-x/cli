360d6f93050a4e30dd839da7112f1a18f0021542
quguiren <quguiren@huawei.com>
2025-03-12 10:25:45 +0800

    code check modify
    Signed-off-by: quguiren <quguiren@huawei.com>

--- ace_tools/lib/src/cli/ace-build/ace-packager/pkgICUData.js
+++ ace_tools/lib/src/cli/ace-build/ace-packager/pkgICUData.js
@@ -23,194 +23,38 @@ const { Platform, platform } = require('../../ace-check/platform');
 const { getAarName, isAppProject } = require('../../util');
 const { getSourceArkuixPath } = require('../../ace-check/checkSource');
 
-const langMap = {
-  'android': {
-    'zh_cn': 'zh_CN',
-    'zh_hk': 'zh_HK',
-    'zh_tw': 'zh_TW',
-    'bo_cn': 'bo_CN',
-    'bo': 'bo',
-    'pt': 'pt',
-    'it': 'it',
-    'ru': 'ru',
-    'fr': 'fr',
-    'de': 'de',
-    'ar': 'ar',
-    'es': 'es',
-    'cs': 'cs',
-    'hr': 'hr',
-    'ja': 'ja',
-    'ko': 'ko',
-    'pl': 'pl',
-    'vi': 'vi',
-    'ms': 'ms',
-    'az_az': 'az_AZ',
-    'az': 'az',
-    'be': 'be',
-    'bg': 'bg',
-    'ca': 'ca',
-    'da': 'da',
-    'el': 'el',
-    'es_us': 'es_US',
-    'et': 'et',
-    'fa': 'fa',
-    'fi': 'fi',
-    'hi': 'hi',
-    'hu': 'hu',
-    'in': 'in',
-    'iw': 'iw',
-    'ka_ge': 'ka_GE',
-    'pt_pt': 'pt_PT',
-    'si_lk': 'si_LK',
-    'sk': 'sk',
-    'ka': 'ka',
-    'si': 'si',
-    'sl': 'sl',
-    'sv': 'sv',
-    'th': 'th',
-    'tur': 'tr',
-    'tr': 'tr',
-    'ur': 'ur',
-    'kk_kz': 'kk_KZ',
-    'kk': 'kk',
-    'nl': 'nl',
-    'sr_latn': 'sr_Latn',
-    'sr': 'sr',
-    'km_kh': 'km_KH',
-    'km': 'km',
-    'sw': 'sw',
-    'lv': 'lv',
-    'lo_la': 'lo_LA',
-    'lo': 'lo',
-    'lt': 'lt',
-    'ro': 'ro',
-    'mk': 'mk',
-    'mn': 'mn',
-    'my_zg': 'my_ZG',
-    'my_mm': 'my_MM',
-    'my': 'my',
-    'nb': 'nb',
-    'ne': 'ne',
-    'tl': 'tl',
-    'bn': 'bn',
-    'uk': 'uk',
-    'zz_zx': 'zz_ZX',
-    'zz': 'zz',
-    'en_gb': 'en_GB',
-    'eu': 'eu',
-    'bs': 'bs',
-    'gl': 'gl',
-    'jv_latn': 'jv_Latn',
-    'jv': 'jv',
-    'ml': 'ml',
-    'mr': 'mr',
-    'ta': 'ta',
-    'uz': 'uz',
-    'ug': 'ug',
-    'mai': 'mai',
-    'mi': 'mi',
-    'am': 'am',
-    'as': 'as',
-    'gu': 'gu',
-    'kn': 'kn',
-    'or': 'or',
-    'pa': 'pa',
-    'te': 'te',
-    'zh': 'zh',
-    'en': 'en',
-    'en_us': 'en_US',
-    'en_ca': 'en_CA',
-  },
-  'ios': {
-    'zh-Hans': 'zh_Hans',
-    'zh-Hans-CN': 'zh_Hans_CN',
-    'zh-HK': 'zh_HK',
-    'zh-Hant': 'zh_Hant',
-    'zh-Hant-HK': 'zh_Hant_HK',
-    'zh-Hant-TW': 'zh_Hant_TW',
-    'bo-CN': 'bo_CN',
-    'bo': 'bo',
-    'pt': 'pt',
-    'it': 'it',
-    'ru': 'ru',
-    'fr': 'fr',
-    'de': 'de',
-    'ar': 'ar',
-    'es': 'es',
-    'cs': 'cs',
-    'hr': 'hr',
-    'ja': 'ja',
-    'ko': 'ko',
-    'pl': 'pl',
-    'vi': 'vi',
-    'ms': 'ms',
-    'az-AZ': 'az_AZ',
-    'be': 'be',
-    'bg': 'bg',
-    'ca': 'ca',
-    'da': 'da',
-    'el': 'el',
-    'es-US': 'es_US',
-    'et': 'et',
-    'fa': 'fa',
-    'fi': 'fi',
-    'hi': 'hi',
-    'hu': 'hu',
-    'id': 'id',
-    'he': 'he',
-    'ka-GE': 'ka_GE',
-    'pt-PT': 'pt_PT',
-    'si-LK': 'si_LK',
-    'sk': 'sk',
-    'sl': 'sl',
-    'sv': 'sv',
-    'th': 'th',
-    'tr': 'tr',
-    'ur': 'ur',
-    'kk-KZ': 'kk_KZ',
-    'nl': 'nl',
-    'sr-Latn': 'sr_Latn',
-    'km-KH': 'km_KH',
-    'sw': 'sw',
-    'lv': 'lv',
-    'lo-LA': 'lo_LA',
-    'lt': 'lt',
-    'ro': 'ro',
-    'mk': 'mk',
-    'mn': 'mn',
-    'my-MM': 'my_MM',
-    'nb': 'nb',
-    'ne': 'ne',
-    'tl': 'tl',
-    'bn': 'bn',
-    'uk': 'uk',
-    'zz-ZX': 'zz_ZX',
-    'en-GB': 'en_GB',
-    'eu': 'eu',
-    'bs': 'bs',
-    'gl': 'gl',
-    'jv': 'jv',
-    'jv-ID': 'jv_ID',
-    'ml': 'ml',
-    'mr': 'mr',
-    'ta': 'ta',
-    'uz': 'uz',
-    'ug': 'ug',
-    'mai': 'mai',
-    'mi': 'mi',
-    'am': 'am',
-    'as': 'as',
-    'gu': 'gu',
-    'kn': 'kn',
-    'or': 'or',
-    'pa': 'pa',
-    'te': 'te',
-    'zh': 'zh',
-    'en': 'en',
-    'en-US': 'en_US',
-    'en-CA': 'en_CA',
-  },
-};
+const langMap = new Map([
+  ['android', new Map([
+    ['zh_cn', 'zh_CN'], ['zh_hk', 'zh_HK'], ['zh_tw', 'zh_TW'], ['bo_cn', 'bo_CN'], ['bo', 'bo'], ['pt', 'pt'],
+    ['it', 'it'], ['ru', 'ru'], ['fr', 'fr'], ['de', 'de'], ['ar', 'ar'], ['es', 'es'], ['cs', 'cs'], ['hr', 'hr'],
+    ['ja', 'ja'], ['ko', 'ko'], ['pl', 'pl'], ['vi', 'vi'], ['ms', 'ms'], ['az_az', 'az_AZ'], ['az', 'az'],
+    ['be', 'be'], ['bg', 'bg'], ['ca', 'ca'], ['da', 'da'], ['el', 'el'], ['es_us', 'es_US'], ['et', 'et'],
+    ['fa', 'fa'], ['fi', 'fi'], ['hi', 'hi'], ['hu', 'hu'], ['in', 'in'], ['iw', 'iw'], ['ka_ge', 'ka_GE'],
+    ['pt_pt', 'pt_PT'], ['si_lk', 'si_LK'], ['sk', 'sk'], ['ka', 'ka'], ['si', 'si'], ['sl', 'sl'], ['sv', 'sv'],
+    ['th', 'th'], ['tur', 'tr'], ['tr', 'tr'], ['ur', 'ur'], ['kk_kz', 'kk_KZ'], ['kk', 'kk'], ['nl', 'nl'],
+    ['sr_latn', 'sr_Latn'], ['sr', 'sr'], ['km_kh', 'km_KH'], ['km', 'km'], ['sw', 'sw'], ['lv', 'lv'],
+    ['lo_la', 'lo_LA'], ['lo', 'lo'], ['lt', 'lt'], ['ro', 'ro'], ['mk', 'mk'], ['mn', 'mn'], ['my_zg', 'my_ZG'],
+    ['my_mm', 'my_MM'], ['my', 'my'], ['nb', 'nb'], ['ne', 'ne'], ['tl', 'tl'], ['bn', 'bn'], ['uk', 'uk'],
+    ['zz_zx', 'zz_ZX'], ['zz', 'zz'], ['en_gb', 'en_GB'], ['eu', 'eu'], ['bs', 'bs'], ['gl', 'gl'],
+    ['jv_latn', 'jv_Latn'], ['jv', 'jv'], ['ml', 'ml'], ['mr', 'mr'], ['ta', 'ta'], ['uz', 'uz'], ['ug', 'ug'],
+    ['mai', 'mai'], ['mi', 'mi'], ['am', 'am'], ['as', 'as'], ['gu', 'gu'], ['kn', 'kn'], ['or', 'or'], ['pa', 'pa'],
+    ['te', 'te'], ['zh', 'zh'], ['en', 'en'], ['en_us', 'en_US'], ['en_ca', 'en_CA'],
+  ])],
+  ['ios', new Map([
+    ['zh-Hans', 'zh_Hans'], ['zh-Hans-CN', 'zh_Hans_CN'], ['zh-HK', 'zh_HK'], ['zh-Hant', 'zh_Hant'],
+    ['zh-Hant-HK', 'zh_Hant_HK'], ['zh-Hant-TW', 'zh_Hant_TW'], ['bo-CN', 'bo_CN'], ['bo', 'bo'], ['pt', 'pt'],
+    ['it', 'it'], ['ru', 'ru'], ['fr', 'fr'], ['de', 'de'], ['ar', 'ar'], ['es', 'es'], ['cs', 'cs'], ['hr', 'hr'],
+    ['ja', 'ja'], ['ko', 'ko'], ['pl', 'pl'], ['vi', 'vi'], ['ms', 'ms'], ['az-AZ', 'az_AZ'], ['be', 'be'],
+    ['bg', 'bg'], ['ca', 'ca'], ['da', 'da'], ['el', 'el'], ['es-US', 'es_US'], ['et', 'et'], ['fa', 'fa'],
+    ['fi', 'fi'], ['hi', 'hi'], ['hu', 'hu'], ['id', 'id'], ['he', 'he'], ['ka-GE', 'ka_GE'], ['pt-PT', 'pt_PT'],
+    ['si-LK', 'si_LK'], ['sk', 'sk'], ['sl', 'sl'], ['sv', 'sv'], ['th', 'th'], ['tr', 'tr'], ['ur', 'ur'],
+    ['kk-KZ', 'kk_KZ'], ['nl', 'nl'], ['sr-Latn', 'sr_Latn'], ['km-KH', 'km_KH'], ['sw', 'sw'], ['lv', 'lv'],
+    ['lo-LA', 'lo_LA'], ['lt', 'lt'], ['ro', 'ro'], ['mk', 'mk'], ['mn', 'mn'], ['my-MM', 'my_MM'], ['nb', 'nb'],
+    ['ne', 'ne'], ['tl', 'tl'], ['bn', 'bn'], ['uk', 'uk'], ['zz-ZX', 'zz_ZX'], ['en-GB', 'en_GB'], ['eu', 'eu'],
+    ['bs', 'bs'], ['gl', 'gl'], ['jv-ID', 'jv_Latn'], ['ml', 'ml'], ['mr', 'mr'], ['ta', 'ta'], ['uz', 'uz'],
+    ['ug', 'ug'], ['mai', 'mai'], ['mi', 'mi'], ['am', 'am'], ['as', 'as'], ['gu', 'gu'], ['kn', 'kn'], ['or', 'or'],
+    ['pa', 'pa'], ['te', 'te'], ['zh', 'zh'], ['en', 'en'], ['en-US', 'en_US'], ['en-CA', 'en_CA'],])]
+]);
 
 function getSubProjectDirName(fileType, projectDir) {
   const iosTypes = ['ios', 'ios-framework', 'ios-xcframework'];
@@ -228,18 +72,18 @@ function getSubProjectDirName(fileType, projectDir) {
 function getLangList(fileType, projectDir, system, newLangList) {
   let langList = [];
   const subProjectNameList = getSubProjectDirName(fileType, projectDir);
-  subProjectNameList.forEach(buildProject => {
+  const resConfigsReg = new RegExp(`^//.*$`);
+  for (const buildProject of subProjectNameList) {
     if (system === 'android') {
-      const androidGradlePath = path.join(projectDir,
-        '.arkui-x/android/' + buildProject + '/build.gradle');
+      const androidGradlePath = path.join(projectDir, `.arkui-x/android/${buildProject}/build.gradle`);
       if (fs.existsSync(androidGradlePath)) {
         let gradleData = fs.readFileSync(androidGradlePath, 'utf-8');
         gradleData = gradleData.trim().split('\n');
         for (const element of gradleData) {
-          if (element.indexOf(`resConfigs`) >= 0) {
-            langList = `[${element.replace('resConfigs', '')}]`
+          if (!resConfigsReg.test(element.trim()) && element.indexOf(`resConfigs`) >= 0) {
+            langList = `[${element.replace('resConfigs', '')}]`;
             langList = JSON.parse(langList);
-          } else if (element.indexOf(`resConfig`) >= 0) {
+          } else if (!resConfigsReg.test(element.trim()) && element.indexOf(`resConfig`) >= 0) {
             langList.push(element.replace(`resConfig`, '').replaceAll('"', '').trim());
           }
         }
@@ -267,7 +111,7 @@ function getLangList(fileType, projectDir, system, newLangList) {
         }
       }
     }
-  });
+  }
   let isFullCopy = false;
   if (system === 'android') {
     const iso6391OrIso6392Lang = new RegExp('^[a-zA-Z]{2,3}$');
@@ -285,19 +129,19 @@ function getLangList(fileType, projectDir, system, newLangList) {
       } else {
         continue;
       }
-      if (langMap[system][icuKey]) {
-        newLangList.push(langMap[system][icuKey]);
+      if (langMap.get(system).has(icuKey)) {
+        newLangList.push(langMap.get(system).get(icuKey));
       } else {
         isFullCopy = true;
       }
     }
   } else if (system === 'ios') {
     for (const item of langList) {
-      if (langMap[system][item]) {
+      if (langMap.get(system).has(item)) {
         if (item === 'my-MM') {
           newLangList.push('my_MM', 'my_ZG');
         } else {
-          newLangList.push(langMap[system][item]);
+          newLangList.push(langMap.get(system).get(item));
         }
       } else {
         isFullCopy = true;
@@ -323,7 +167,6 @@ function generateDataFilterFile(dataFilterFilePath, langList) {
     fs.writeFileSync(dataFilterFilePath, JSON.stringify(langList, null, 2));
     return true;
   } catch (err) {
-    console.error('generate data_filter.json err:', err);
     throw new Error('generate filter.json err.');
   }
 }
@@ -472,6 +315,7 @@ function copyDat(projectDir, system, fileType, depMap) {
   }
   const srcPath = path.join(buildOutDir, 'icudt72l.dat');
   execCopyDatCmd(cmds, srcPath, destPath);
+  return true;
 }
 
 module.exports = { copyDat };
