a0f2ac9e1c507b45d9e95ab8c61bf8e3ca013289
yanjunliang_001 <yanjunliang@h-partners.com>
2026-06-03 17:18:17 +0800

    【6.1】回退点分制修改
    Signed-off-by: yanjunliang@h-partners.com <yanjunliang@h-partners.com>
    
    
    revert merge request: 【6.1】回退点分制修改
    
    8b9f63f63dde151006916cdf2f88fb6a3cde44e9
    
    This revert merge request !136

--- ace_tools/lib/src/cli/ace-build/ace-compiler/index.js
+++ ace_tools/lib/src/cli/ace-build/ace-compiler/index.js
@@ -37,7 +37,7 @@ const { copyLibraryToProject } = require('../ace-packager/copyLibraryToProject')
 const analyze = require('../ace-analyze/index');
 const { createAndroidAndIosBuildArkTSShell } = require('../../ace-create/util');
 const { getSourceArkuixPath } = require('../../ace-check/checkSource');
-const { getCreatedPlatforms, getDevEcoNodePath } = require('../../util');
+const { getCreatedPlatforms, changeVersion, getDevEcoNodePath } = require('../../util');
 
 let projectDir;
 let arkuiXSdkPath;
@@ -322,7 +322,9 @@ function copyLibsToBuild(moduleListSpecified, buildPath, cmd) {
 function runGradle(fileType, cmd, moduleList, commonModule, testModule) {
   let cmds = [`cd ${projectDir}`];
   let buildCmd = `./hvigorw`;
-  if (Number(getSdkVersion(projectDir)) >= 12) {
+  let projectSdkVersion = getSdkVersion(projectDir);
+  let sdkVersionResult = changeVersion(projectSdkVersion);
+  if (sdkVersionResult >= 12) {
     if (getIntergrateHvigorw()) {
       buildCmd = `"${getIntergrateHvigorw()}"`;
     } else {
@@ -341,7 +343,7 @@ function runGradle(fileType, cmd, moduleList, commonModule, testModule) {
     console.log('\x1b[33m%s\x1b[0m', 'WARN: The ohpm version is too low, which may cause the download failure of the ohos third-party library. Please set the ohpm version to a value greater than or equal to 6.0.1');
   } 
   cmds.push(`"${ohpmPath}" install`);
-  if (platform !== Platform.Windows && Number(getSdkVersion(projectDir)) < 12) {
+  if (platform !== Platform.Windows && sdkVersionResult < 12) {
     cmds.push(`chmod 755 hvigorw`);
   }
   let gradleMessage;
@@ -393,7 +395,7 @@ function runGradle(fileType, cmd, moduleList, commonModule, testModule) {
     if (cmd.debug) {
       buildModeStr = '';
     }
-    const buildtarget = 'default@CompileArkTS --no-parallel' + moduleStr + buildModeStr;
+    const buildtarget = 'default@CompileArkTS --no-parallel' + moduleStr + buildModeStr + ' --no-daemon';
     let testbBuildtarget = '';
     if (cmd.debug && testModule) {
       const moduleTestStr = '-p module=' + testModule.join('@ohosTest,') + '@ohosTest';
--- ace_tools/lib/src/cli/util/index.js
+++ ace_tools/lib/src/cli/util/index.js
@@ -474,6 +474,7 @@ function getSdkVersionMap() {
     ['22', new Map([['devEcoVersion', '6.0.2'], ['compileSdkVersion', '6.0.2(22)'], ['compatibleSdkVersion', '6.0.2(22)'], ['modelVersion', '6.0.2'], ['runtimeOS', 'HarmonyOS'], ['hvigor-ohos-arkui-x-plugin', '4.22.3']])],
     ['23', new Map([['devEcoVersion', '6.1.0'], ['compileSdkVersion', '6.1.0(23)'], ['compatibleSdkVersion', '6.1.0(23)'], ['modelVersion', '6.1.0'], ['runtimeOS', 'HarmonyOS'], ['hvigor-ohos-arkui-x-plugin', '4.23.6']])],
     ['24', new Map([['devEcoVersion', '6.1.1'], ['compileSdkVersion', '6.1.1(24)'], ['compatibleSdkVersion', '6.1.1(24)'], ['modelVersion', '6.1.1'], ['runtimeOS', 'HarmonyOS'], ['hvigor-ohos-arkui-x-plugin', '4.24.1']])],
+    ['26.0.0', new Map([['devEcoVersion', '26.0.0'], ['compileSdkVersion', '26.0.0'], ['compatibleSdkVersion', '26.0.0'], ['modelVersion', '26.0.0'], ['runtimeOS', 'HarmonyOS'], ['hvigor-ohos-arkui-x-plugin', '4.24.1']])],
   ]);
   return sdkVersionMap;
 }
@@ -665,7 +666,7 @@ function getModuleType(projectDir, modulePath) {
       } else {
         moduleType = 'EmptyAbility';
       }
-    } else if (hvigorInfo.includes('hspTasks')) {
+    } else if (hvigorInfo.includes('HspTasks')) {
       if (checkInfo.includes('CMakeLists.txt')) {
         moduleType = 'ShareC++';
       } else {
@@ -861,6 +862,14 @@ function isSupportedOperatorType(platforms, type) {
   return true;
 }
 
+function changeVersion(nowVersion) {
+  let versionResult = Number(nowVersion);
+  if (Number.isNaN(versionResult)) {
+    versionResult = Number(nowVersion.split('.')[0]);
+  }
+  return versionResult;
+}
+
 function getDevEcoNodePath() {
   let devEcoNodePath = `${devEcoStudioDir}\\tools\\node`;
   if (platform === Platform.MacOS) {
@@ -912,5 +921,6 @@ module.exports = {
   getCreatedPlatforms,
   isSupportedOperatorType,
   getIsArkuixProject,
+  changeVersion,
   getDevEcoNodePath,
 };
